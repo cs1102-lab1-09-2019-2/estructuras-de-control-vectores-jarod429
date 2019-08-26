8d7c8b923d44890058ff6353990d4923a74eec8b
jarod429 <54276838+jarod429@users.noreply.github.com>
2019-08-26 15:40:41 -0500

    Update ejercicio3.cpp

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,25 +1,35 @@
-
+#include<iostream>
+#include<vector>
+using namespace std;
 int mediana(vector<int> &lista);
 vector<int> leerConsola();
 
 vector<int> leerConsola() {
-    //Ingrese un numero positivo, escriba -1 si ya no quiere ingresar mas numeros:
-}
-
+vector<int> Ans;
+  int numer = 0;
+  while(numer!=-1){ cout<<"ingrese un numero ";
+      cin>>numer;
+      Ans.push_back(numer);
+ }
+  Ans.pop_back();
+  return Ans;}//Ingrese un numero positivo, escriba -1 si ya no quiere ingresar  mas numeros:
 int mediana(vector<int> &lista) {
-    /*
+     int median=0;
+    int n=lista.size();
+    if(n%2==0){
+      median=lista[n/2 -1] +lista[n/2];}
+    else median=lista[(n-1)/2];/*
     Si el vector es 3, 9, 11, 15
     Si el número de observaciones es par,
      la mediana corresponde al promedio de los dos valores centrales.
      Por ejemplo, en la muestra 3, 9, 11, 15, la mediana es (9+11)/2=10.
      (v[4/2 -1] + v[4/2])/2 = (v[1] + v[2])/2 = (9+11)/2
-
     Si el vector es 3, 9, 11, 15, 16
     Si el numero de observaciones es impar,
      Por ejemplo, en la muestra 3, 9, 11, 15, 16, la mediana es 11
      v[4/2] = v[2] = 11
      */
-    return null;
+    return median;
 }
 
 int main() {
