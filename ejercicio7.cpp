e9e4c64ceae8515424b8cbfe6eb6cad7a03391a0
jarod429 <54276838+jarod429@users.noreply.github.com>
2019-08-29 08:35:44 -0500

    Update ejercicio7.cpp

--- ejercicio7.cpp
+++ ejercicio7.cpp
@@ -21,18 +21,13 @@
 
 #include <iostream>
 #include <stdexcept> //Validar en la division por 0
-using std::cout;
-using std::cin;
-using std::invalid_argument;
-
+using namespace std;
 void mostrarMenu();
 int sumar(int a, int b);
 int restar(int a, int b);
 int multiplicar(int a, int b);
 float dividir(int a, int b);
 int modulo(int a, int b);
-
-
 void mostrarMenu() {
     cout<<"***************************************************\n";
     cout<<"                     MENU                          \n";
@@ -41,9 +36,74 @@ void mostrarMenu() {
     cout<<"  2.- Resta\n";
     cout<<"  3.- Multiplicar\n";
     cout<<"  4.- Dividir\n";
-    cout<<"  6.- Modulo\n";
+    cout<<"  5.- Modulo\n";
 }
+int sumar(int a,int b){
+  int c;
+  c=a+b; 
+  return c;
+}
+int resta(int a,int b){
+   int c;
+  c=a-b; 
+  return c;
 
+}
+int multiplicar(int a,int b){
+   int c;
+  c=a*b; 
+  return c;
+
+}
+float dividir(int a,int b){
+   int c;
+  c=a/b; 
+  return c;
+
+}
+int modulo(int a,int b){
+   int c;
+  c=a%b; 
+  return c;
+
+}
 int main() {
+  int a;
+  int b;
+  int opcion;
+  mostrarMenu();
+  cout<<"ingresar una opcion";
+  cin>>opcion;
+  switch(opcion){
+    case 1:{
+      cout<<"ingrese  dos números separados por un espacio";
+      cin>>a;
+      cin>>b;
+      cout<<"Resultado:"<<sumar(a,b);
+      break;
+    }
+    case 2:{
+      cout<<"ingrese  dos números separados por un espacio";
+      cin>>a;
+      cin>>b;
+      cout<<"Resultado:"<<resta(a,b);
+      break;
+    }
+    case 3:{cout<<"ingrese  dos números separados por un espacio";
+      cin>>a;
+      cin>>b;
+      cout<<"Resultado:"<<multiplicar(a,b);
+      break;}
+    case 4:{cout<<"ingrese  dos números separados por un espacio";
+      cin>>a;
+      cin>>b;
+      cout<<"Resultado:"<<dividir(a,b);
+      break;}
+    case 5:{cout<<"ingrese  dos números separados por un espacio";
+      cin>>a;
+      cin>>b;
+      cout<<"Resultado:"<<modulo(a,b);
+      break;}
+  }
     return 0;
 }
