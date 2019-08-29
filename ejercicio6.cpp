c453816cdfbd800134d97ebf6e3b7e728561fb23
jarod429 <54276838+jarod429@users.noreply.github.com>
2019-08-29 08:50:18 -0500

    Update ejercicio6.cpp

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -4,3 +4,25 @@
  Y encuentra el mayor valor: 30
 
  */
+#include <iostream>
+using namespace std;
+int main(){
+int a;
+int b;
+int c;
+cout<<"ingrese tres numeros separados por un espacio: ";
+cin>>a;
+cin>>b;
+cin>>c;
+if(a>b&a>c){
+  cout<<"el mayor valor:"<<a;
+}
+else if (b>a&b>c){
+  cout<<"el mayor valor:"<<b;
+
+}
+else if (c>a&c>b){
+  cout<<"el mayor valor:"<<c;
+
+}
+}
